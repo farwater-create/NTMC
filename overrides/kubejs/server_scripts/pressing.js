01db71cb88d45f079c27b348213b2fd17e6f6f6a
Janko <161398674+Janko1902@users.noreply.github.com>
2024-11-23 02:21:32 +0100

    incomplete iron tools

---
+++ overrides/kubejs/server_scripts/pressing.js
@@ -0,0 +1,8 @@
+ServerEvents.recipes((e) => {
+    e.recipes.create.pressing("minecraft:iron_pickaxe", "ntmc:incomplete_iron_pickaxe");
+    e.recipes.create.pressing("minecraft:iron_axe", "ntmc:incomplete_iron_axe");
+    e.recipes.create.pressing("minecraft:iron_shovel", "ntmc:incomplete_iron_shovel");
+    e.recipes.create.pressing("minecraft:iron_sword", "ntmc:incomplete_iron_sword");
+    e.recipes.create.pressing("minecraft:iron_hoe", "ntmc:incomplete_iron_hoe");
+
+});
---
+++ overrides/kubejs/startup_scripts/items.js
@@ -0,0 +1,7 @@
+StartupEvents.registry("item", (e) => {
+    e.create("ntmc:incomplete_iron_pickaxe");
+    e.create("ntmc:incomplete_iron_axe");
+    e.create("ntmc:incomplete_iron_shovel");
+    e.create("ntmc:incomplete_iron_sword");
+    e.create("ntmc:incomplete_iron_hoe");
+});
