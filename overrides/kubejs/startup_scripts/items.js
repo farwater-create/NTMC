ef4e9bd8059def4093f61f4b2bd7ab2860f689e6
Janko <161398674+Janko1902@users.noreply.github.com>
2024-11-23 02:37:34 +0100

    make incomplete iron tools unstackable

--- overrides/kubejs/startup_scripts/items.js
+++ overrides/kubejs/startup_scripts/items.js
@@ -1,7 +1,7 @@
 StartupEvents.registry("item", (e) => {
-    e.create("ntmc:incomplete_iron_pickaxe");
-    e.create("ntmc:incomplete_iron_axe");
-    e.create("ntmc:incomplete_iron_shovel");
-    e.create("ntmc:incomplete_iron_sword");
-    e.create("ntmc:incomplete_iron_hoe");
+    e.create("ntmc:incomplete_iron_pickaxe").maxStackSize(1);
+    e.create("ntmc:incomplete_iron_axe").maxStackSize(1);
+    e.create("ntmc:incomplete_iron_shovel").maxStackSize(1);
+    e.create("ntmc:incomplete_iron_sword").maxStackSize(1);
+    e.create("ntmc:incomplete_iron_hoe").maxStackSize(1);
 });
