ea0eee3c8ab50845e16f7319da8ce9854fba7f3f
Janko <161398674+Janko1902@users.noreply.github.com>
2024-11-23 02:54:24 +0100

    remove old iron tool recipes

---
+++ overrides/kubejs/server_scripts/removals.js
@@ -0,0 +1,11 @@
+const recipeRemovals = [
+    { output: "minecraft:iron_pickaxe"},
+    { output: "minecraft:iron_axe"},
+    { output: "minecraft:iron_shovel"},
+    { output: "minecraft:iron_sword"},
+    { output: "minecraft:iron_hoe"},
+];
+
+ServerEvents.recipes((e) => {
+    recipeRemovals.forEach((recipe) => e.remove(recipe));
+});
