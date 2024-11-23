78a9e7ec8dd50cb31213e51ac066865e0ac1a1d7
Janko <161398674+Janko1902@users.noreply.github.com>
2024-11-23 17:53:17 +0100

    animations

--- overrides/kubejs/startup_scripts/fluid.js
+++ overrides/kubejs/startup_scripts/fluid.js
@@ -1,13 +1,13 @@
 StartupEvents.registry('fluid', (e) => {
     e.create('liquid_conductor')
       .displayName('Liquid Conductor')
-      .stillTexture('kubejs:block/liquid_conductor_still')
+      .stillTexture('ntmc:block/liquid_conductor_still')
       .flowingTexture('ntmc:block/liquid_conductor_flow')
       .bucketColor(0xffffff);
 
     e.create('liquid_redstone')
       .displayName('Liquid Redstone')
-      .stillTexture('kubejs:block/liquid_redstone_still')
+      .stillTexture('ntmc:block/liquid_redstone_still')
       .flowingTexture('ntmc:block/liquid_redstone_flow')
       .bucketColor(0xffffff);
 });
