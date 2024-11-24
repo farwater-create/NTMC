91fc64fe2ec9c1734efd4a3d2e52f511672cf64e
Janko <161398674+Janko1902@users.noreply.github.com>
2024-11-24 17:53:44 +0100

    Update sequenced_assembly.js

--- overrides/kubejs/server_scripts/sequenced_assembly.js
+++ overrides/kubejs/server_scripts/sequenced_assembly.js
@@ -1,13 +1,14 @@
 ServerEvents.recipes((e) => {
+  let ti = Item.of(t)
   let t = "kubejs:incomplete_controller";
   e.recipes.create
     .sequenced_assembly(
       [Item.of("ae2:controller")],
       Item.of("create:electron_tube"),
       [
-        e.recipes.create.deploying(Item.of(t), [Item.of(t), Item.of("ae2:certus_quartz_crystal")]),
-        e.recipes.create.deploying(Item.of(t), [Item.of(t), Item.of("ntmc:conduction_ingot")]),
-        e.recipes.create.filling(Item.of(t), [Fluid.of("kubejs:liquid_conductor", 50 * 81), Item.of(t)]),
+        e.recipes.create.deploying(ti, [ti, Item.of("ae2:certus_quartz_crystal")]),
+        e.recipes.create.deploying(ti, [ti, Item.of("ntmc:conduction_ingot")]),
+        e.recipes.create.filling(ti, [Fluid.of("kubejs:liquid_conductor", 50 * 81), ti]),
       ]
     )
     .transitionalItem(t)
