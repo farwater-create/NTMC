7829082428e871d357e4b53e29ac44682d19c017
TyrDrop <tyr@serpentkiss.com>
2024-11-27 12:35:04 -0500

    kubJS script help

--- overrides/kubejs/server_scripts/tinkers_construct.js
+++ overrides/kubejs/server_scripts/tinkers_construct.js
@@ -1,44 +1,44 @@
-ServerEvents.recipes,((e) => {
+ServerEvents.recipes((e) => {
     e.custom({
-        type: "tconstruct:alloy",
-        inputs: [
+        "type": "tconstruct:alloy",
+        "inputs": [
             {
-                amount: 144,
-                fluid: "hephaestus:molten_gold"
+                "amount": 144 * 81,
+                "fluid": "hephaestus:molten_gold"
             },
             {
-                amount: 144,
-                fluid: "ntmc:molten_redstone"
+                "amount": 144 * 81,
+                "fluid": "ntmc:molten_redstone"
             }
         ],
-        result: { fluid: "ntmc:molten_conductor", amount: 288},
-        temperature: 1400
+        "result": { "fluid": "ntmc:molten_conductor", "amount": 288 * 81},
+        "temperature": 1400
     });
 
     e.custom({
-        type: 'tconstruct:casting_table',
-        cast: {
-            tag: 'tconstruct:casts/multi_use/ingot'
+        "type": 'tconstruct:casting_table',
+        "cast": {
+            "tag": 'tconstruct:casts/multi_use/ingot'
         },
-        cooling_time: 47,
-        fluid: {
-            amount: 144,
-            fluid: 'ntmc:molten_conductor'
+        "cooling_time": 47,
+        "fluid": {
+            "amount": 90 * 81,
+            "fluid": 'ntmc:molten_conductor'
         },
-        result: {
-            item: 'ntmc:conductor_ingot'
+        "result": {
+            "item": 'ntmc:conductor_ingot'
         }
     });
 
     e.custom({
-        type: "tconstruct:melting",
-        inputs: [
+        "type": "tconstruct:melting",
+        "inputs": [
             {
-                tag: "minecraft:redstone_dust"
+                "tag": "c:redstone_dust"
             }
         ],
-        result: { fluid: "ntmc:molten_redstone", amount: 144},
-        temperature: 700,
-        time: 60
+        "result": { "fluid": "ntmc:molten_redstone", "amount": 144 * 81},
+        "temperature": 700,
+        "time": 60
     });
 });
